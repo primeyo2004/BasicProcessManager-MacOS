4a4a6dc1a3d9aeff83c18e41ac5802b0fd56bd4a
prime <primeyo2004@yahoo.com>
2017-03-06 00:47:13 +0800

    Minor fixes and improvements

---
+++ ProcessWindow.m
@@ -0,0 +1,13 @@
+//
+//  NSProcessWindow.m
+//  BasicProcessManager
+//
+//  Created by Jeune Prime Origines on 5/3/17.
+//  Copyright © 2017 Jeune Prime Origines. All rights reserved.
+//
+
+#import "NSProcessWindow.h"
+
+@implementation NSProcessWindow
+
+@end
